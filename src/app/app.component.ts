63c995a448557a95099006c2aa001a6c60a937f9
Leonardo Gomes Martins <leonardo.gomes@anm.gov.br>
2023-11-15 12:48:39 -0300

    feat: update design of pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent { }
 
 // TODO:
-// 1. Update Design of pages
-// 2. Update Background Color
-// 3. Create Error Message
-// 4. Create Loading Component Or Placeholder Component
-// 5. Create Not Found Component
-// 6. Create Filter By Type And Ability
+// 1. Create Error Message
+// 2. Create Loading Component Or Placeholder Component
+// 3. Create Not Found Component
+// 4. Create Filter By Type And Ability
